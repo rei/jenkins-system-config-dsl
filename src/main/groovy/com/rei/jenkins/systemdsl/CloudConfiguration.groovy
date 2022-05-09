1c56de445bb17720b5615fc4c781c83265a46b8f
Evan Shioyama <eshioya@rei.com>
2022-05-09 16:17:34 -0700

    Fix typo

--- src/main/groovy/com/rei/jenkins/systemdsl/CloudConfiguration.groovy
+++ src/main/groovy/com/rei/jenkins/systemdsl/CloudConfiguration.groovy
@@ -277,7 +277,7 @@ class CloudConfiguration extends DslSection {
                 void slaveCommandPrefix(String slaveCommandPrefix) { this.slaveCommandPrefix = slaveCommandPrefix }
                 void slaveCommandSuffix(String slaveCommandSuffix)  {this.slaveCommandSuffix = slaveCommandSuffix}
                 void sshPort(int sshPort) { this.sshPort = sshPort }
-                void bootDelay(Sttring bootDelay) { this.bootDelay = bootDelay }
+                void bootDelay(String bootDelay) { this.bootDelay = bootDelay }
 
                 UnixData getTypeData() {
                     return new UnixData(rootCommandPrefix, slaveCommandPrefix, slaveCommandSuffix, sshPort as String, bootDelay)
